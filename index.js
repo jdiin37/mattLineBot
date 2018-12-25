dad83d0420cd3da89ad2b6e55c9c60cf0fcef178
matt <matt@advmeds.com>
2018-12-25 15:30:27 +0800

    eeert

--- index.js
+++ index.js
@@ -4,23 +4,18 @@ var functionPool = require('./myLib/fucntionPool');
 
 
 var bot = linebot({
-    channelId: process.env.channelId,
-    channelSecret: process.env.channelSecret,
-    channelAccessToken: process.env.channelAccessToken
+  channelId: 1633532932,
+  channelSecret: "48cd941e2dfa02378ac35b22f13d96d7",
+  channelAccessToken: "aewswqqjsMoz9s0Aw8VePSimZwKm8u6vugPt1n8Kv5x/8BMJdXWf5yiZEPvpPwSfTjQrg02wnwjT7EeJnQlIj/wS3s+f8RHat7zZE1qaNAdlab/wunz7oxFeN1U/3C/jnrDkZKMauMRgSczGNcjJJAdB04t89/1O/w1cDnyilFU="
 });
-
-const app = express();
-const linebotParser = bot.parser();
-// app.get("/", function (req, res) { 
-//   res.send("Hello LineBot");
-// });
-app.post('/', linebotParser);
-
 
 bot.on('message', function(event) {
     console.log(event); //把收到訊息的 event 印出來看看
 });
   
+const app = express();
+const linebotParser = bot.parser();
+app.post('/', linebotParser);
 
 //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
 var server = app.listen(process.env.PORT || 8080, function() {
