093266b0159936264820be36a10846e3384a4d7b
matt <matt@advmeds.com>
2018-12-26 15:59:01 +0800

    fghfgh

--- index.js
+++ index.js
@@ -32,6 +32,38 @@ var countSum = 0;
 
 var ModType = '';
 
+var x = Math.floor(Math.random() * 99) + 1;
+var intMax = 100;
+var intMin = 0;
+var leaveCount = 5;
+var PWDGame_startMsg = "終極密碼" + intMin+"~" + intMax+",還可猜" + leaveCount + "次";
+
+var PWDGame = function(pwd){
+  
+  var rtnMsg = '';
+  if (pwd <= intMin || pwd >= intMax || isNaN(pwd))
+  {
+      rtnMsg = "請輸入介於" + intMin + "~" + intMax + "之間的數";      
+  }
+
+  if (pwd > x) {
+      intMax = pwd;
+      leaveCount-- ;
+      rtnMsg = PWDGame_startMsg;
+  }
+  if (pwd < x) {
+      intMin = pwd;
+      leaveCount-- ;
+      rtnMsg = PWDGame_startMsg;
+  }
+
+  if (pwd == x) {
+      rtnMsg = "恭喜答對了";
+  }
+    
+  return rtnMsg;
+}
+
 var isOnhour = function (){  
   myTimeOut = setTimeout(function(){    
       if(functionPool.isOnhour()){
@@ -57,35 +89,63 @@ bot.on('message', function(event) {
       var msg = event.message.text.toLowerCase() ;
       var replyMsg = '"' + msg + '?"' + functionPool.showIAmGroot(); 
       
-      if (msg.indexOf('time') != -1) {
-        GotIt = true;
-        bot.push(userId,functionPool.checkTime());              
+      if(ModType == 'PWD'){
+        if(msg.indexOf('exitPWD') != -1){
+          GotIt = true;
+          ModType = '';
+          replyMsg = "已離開終極密碼-------";
+          bot.push(userId,replyMsg);           
+        }
+
+        if (isNaN(msg)) {
+          GotIt = true;
+          bot.push(userId,PWDGame(msg));  
+        }else{
+          GotIt = true;
+          replyMsg = "請輸入數字";
+          bot.push(userId,replyMsg);           
+        }
+
+      }else{
+        if (msg.indexOf('playPWD') != -1) {
+          GotIt = true;
+          ModType = 'PWD'              
+          bot.push(userId,"開始玩終極密碼!!-------");
+          bot.push(userId,PWDGame_startMsg); 
+        }
+
+
+        if (msg.indexOf('time') != -1) {
+          GotIt = true;
+          bot.push(userId,functionPool.checkTime());              
+        }
+  
+        if (msg.indexOf('search') != -1){
+          //replyMsg = functionPool.googleSearch();
+          GotIt = true;
+          bot.push(userId,"你要搜尋什麼?");
+        }
+  
+        if(msg.indexOf('start') != -1){
+          GotIt = true;
+          bot.push(userId,"開始!!");
+          isOnhour();
+        }
+  
+  
+        if(msg.indexOf('reset') != -1){
+          GotIt = true;
+          countSum = 0 ;
+          bot.push(userId,"重新設定完成!!");
+        }
+        
+        if(msg.indexOf('stop') != -1){
+          GotIt = true;
+          bot.push(userId,"停止!!");
+          clearTimeout(myTimeOut);
+        }
       }
 
-      if (msg.indexOf('search') != -1){
-        //replyMsg = functionPool.googleSearch();
-        GotIt = true;
-        bot.push(userId,"你要搜尋什麼?");
-      }
-
-      if(msg.indexOf('start') != -1){
-        GotIt = true;
-        bot.push(userId,"開始!!");
-        isOnhour();
-      }
-
-
-      if(msg.indexOf('reset') != -1){
-        GotIt = true;
-        countSum = 0 ;
-        bot.push(userId,"重新設定完成!!");
-      }
-      
-      if(msg.indexOf('stop') != -1){
-        GotIt = true;
-        bot.push(userId,"停止!!");
-        clearTimeout(myTimeOut);
-      }
 
 
       if(!GotIt){
